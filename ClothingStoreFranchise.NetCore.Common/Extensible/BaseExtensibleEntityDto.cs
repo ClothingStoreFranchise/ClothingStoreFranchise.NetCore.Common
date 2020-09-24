fe6eb3891559f0e68dd031ccf7192da1fd054d97
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-24 13:29:31 +0200

    extensible dto

---
+++ ClothingStoreFranchise.NetCore.Common/Extensible/BaseExtensibleEntityDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ClothingStoreFranchise.NetCore.Common.Extensible
+{
+    public abstract class BaseExtensibleEntityDto
+    {
+        public abstract string ExtensibleEntityName { get; }
+
+        public Dictionary<string, object> ExtendedData { get; set; }
+
+        protected BaseExtensibleEntityDto()
+        {
+            ExtendedData = new Dictionary<string, object>();
+        }
+    }
+}
