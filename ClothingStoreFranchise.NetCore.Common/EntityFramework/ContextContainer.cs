1602dce880e722eda2e9bcc1e0864be29ee487ed
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-24 13:29:57 +0200

    EF commons clases.

---
+++ ClothingStoreFranchise.NetCore.Common/EntityFramework/ContextContainer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingStoreFranchise.NetCore.Common.EntityFramework
+{
+    public interface IContextContainer
+    {
+        DbContext Context { get; }
+    }
+
+    public interface ITypedEFContextContainer<TContext> : IContextContainer where TContext : DbContext
+    {
+    }
+
+    public class ContextContainer<TContext> : ITypedEFContextContainer<TContext> where TContext : DbContext
+    {
+        public DbContext Context { get; private set; }
+
+        public ContextContainer(TContext context)
+        {
+            Context = context;
+        }
+    }
+}
