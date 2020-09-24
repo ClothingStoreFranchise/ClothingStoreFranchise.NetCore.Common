208a4526fb5b6b3dd90c389e66d490eb12962b94
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-24 13:27:06 +0200

    Mapper provider and common mapper extension class.

---
+++ ClothingStoreFranchise.NetCore.Common/Mapper/BaseMapperProvider.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClothingStoreFranchise.NetCore.Common.Mapper
+{
+    public interface IMapperProvider
+    {
+        ICollection<Assembly> Assemblies { get; }
+    }
+
+    public abstract class BaseMapperProvider : IMapperProvider
+    {
+        public ICollection<Assembly> Assemblies { get; private set; }
+
+        protected BaseMapperProvider()
+        {
+            Assemblies = new List<Assembly>();
+            AddAssemblies();
+        }
+
+        private void AddAssemblies()
+        {
+            Assembly assembly = GetType().Assembly;
+            string assemblyName = assembly.GetName().Name;
+            string assemblyPrefix = assemblyName.Substring(0, assemblyName.IndexOf('.'));
+
+            Assemblies.Add(assembly);
+        }
+
+        protected virtual MapperConfiguration CreateConfiguration()
+        {
+            return new MapperConfiguration(cfg => { });
+        }
+
+        public AutoMapper.Mapper GetMapper()
+        {
+            return new AutoMapper.Mapper(CreateConfiguration());
+        }
+    }
+}
